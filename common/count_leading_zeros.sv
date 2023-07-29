6f287c6e6d6b0e07b97523c2e55971d4cb80f20f
sean galloway <sean.galloway@outlook.com>
2023-07-29 06:40:37 -0700

    tweaks

--- common/count_leading_zeros.sv
+++ common/count_leading_zeros.sv
@@ -1,22 +1,32 @@
 `timescale 1ns / 1ps
 
-module count_leading_zeros #(parameter int WIDTH = 32)
-(
-    input logic [WIDTH-1:0]          data,
-    output logic [$clog2(WIDTH)-1:0] leading_zeros_count
-);
+// Taken from StackExchange:
+// https://electronics.stackexchange.com/questions/196914/verilog-synthesize-high-speed-leading-zero-count
+module count_leading_zeros (ref_vector, dout);
+    parameter  REF_VECTOR_WIDTH=32;
+    localparam DOUT_WIDTH    = $clog2(REF_VECTOR_WIDTH)+1;
+    localparam DOUT_LR_WIDTH = DOUT_WIDTH-1;
+    input  [REF_VECTOR_WIDTH-1:0]  ref_vector;
 
-    logic [$clog2(WIDTH)-1:0] leading_zeros;
+    output [DOUT_WIDTH-1:0]  dout;
 
-    always_comb begin
-        leading_zeros = WIDTH - 1;
-        for (int i = WIDTH-1; i >= 0; i--) begin
-            if (data[i] == 1'b1) begin
-                leading_zeros = i;
-            end
+    wire  [DOUT_LR_WIDTH-1:0]  dout_r;
+    wire  [DOUT_LR_WIDTH-1:0]  dout_l;
+
+    wire  [REF_VECTOR_WIDTH/2-1:0]  ref_vector_r;
+    wire  [REF_VECTOR_WIDTH/2-1:0]  ref_vector_l;
+
+    generate 
+        if (REF_VECTOR_WIDTH  == 2)
+            assign dout = (ref_vector == 2'b00) ? 'd2 : 
+                          (ref_vector == 2'b01) ? 'd1 : 0;
+        else begin 
+            assign ref_vector_l = ref_vector[REF_VECTOR_WIDTH-1:REF_VECTOR_WIDTH/2];
+            assign ref_vector_r = ref_vector[REF_VECTOR_WIDTH/2-1:0];
+            count_leading_zeros #(REF_VECTOR_WIDTH/2) u_nv_clz_l(ref_vector_l, dout_l);
+            count_leading_zeros #(REF_VECTOR_WIDTH/2) u_nv_clz_r(ref_vector_r, dout_r);
+            assign dout = (~dout_l[DOUT_LR_WIDTH-1]) ? {dout_l [DOUT_LR_WIDTH-1] & dout_r [DOUT_LR_WIDTH-1], 1'b0                    , dout_l[DOUT_LR_WIDTH-2:0]} :
+                                                       {dout_l [DOUT_LR_WIDTH-1] & dout_r [DOUT_LR_WIDTH-1], ~dout_r[DOUT_LR_WIDTH-1], dout_r[DOUT_LR_WIDTH-2:0]};
         end
-    end
-
-    assign leading_zeros_count = WIDTH - leading_zeros - 1;
-
-endmodule : count_leading_zeros
+    endgenerate
+endmodule
--- common/find_first_set.sv
+++ common/find_first_set.sv
@@ -1,18 +1,17 @@
 `timescale 1ns / 1ps
 
 module find_first_set
-#(parameter int WIDTH = 32)
+#(parameter      int WIDTH = 32,
+  localparameter int N = $clog2(WIDTH))
 (
-    input  logic [WIDTH-1:0]         data,
-    output logic [$clog2(WIDTH)-1:0] first_set_index
+    input  logic [WIDTH-1:0]   data,
+    output logic [N-1:0]       first_set_index
 );
-
-    localparam  N = $clog2(WIDTH);
 
     function automatic logic [N-1:0] ffs(input logic [WIDTH-1:0] vector);
         logic [N-1:0] location;
 
-        location = '{>>{N{1}}};
+        location = '{N{1'b1}};
 
         for (int i = 0; i < CLIENTS; i++)
             if (vector[i] == 1'b1)
