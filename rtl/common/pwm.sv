7fe37d317ba7dd11379c43b926055ed0716194e7
sean-galloway <sean.galloway@outlook.com>
2023-08-12 21:20:34 -0700

    naming updates

---
+++ rtl/common/pwm.sv
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+module pwm #(parameter WIDTH=8)(
+    input                     i_clk, i_rst_n,   // clock and reset
+    input                     i_start,          // i_start the pwm
+    input [WIDTH-1:0]         i_duty,           // max count for keeping PWM high
+    input [WIDTH-1:0]         i_period,         // max total count
+    input [WIDTH-1:0]         i_repeat_count,   // repeat the counter
+    output logic              ow_done,
+    output logic              o_pmw             // done and the pwm signal
+);
+
+    logic [WIDTH-1:0] r_count;
+    logic [WIDTH-1:0] r_repeat_value;
+    logic             w_count_met, w_counting, w_done_repeat;
+
+    assign w_count_met = (r_count == i_period-1);
+    assign w_counting = (i_start && !ow_done);
+    assign w_done_repeat = (i_repeat_count != 'b0) && (r_repeat_value == i_repeat_count);
+    assign ow_done = w_done_repeat;
+
+    // count handling
+    always_ff @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n)        r_count <= 'b0;
+        else if  (w_count_met) r_count <= 'b0;
+        else if (w_counting)   r_count <= r_count + 1;
+    end
+    
+    // repeat w_counting
+    always_ff @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n)                    r_repeat_value <= 'b0;
+        else if  (w_counting && w_count_met) r_repeat_value <= r_repeat_value + 'b1;
+    end
+
+    // pwm signal handling
+    always_comb begin
+        if (r_count < i_duty && !ow_done) o_pmw = 1;
+        else o_pmw = 0;
+    end
+
+// synopsys translate_off
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, pwm);
+end
+// synopsys translate_on
+
+endmodule : pwm
---
+++ val/common_force_tb/pwm/testbench.py
@@ -0,0 +1,32 @@
+
+import queue
+from cocotb.clock import Clock
+from cocotb.triggers import Timer
+import cocotb
+
+@cocotb.test()
+async def test_fifo(dut):
+    q1 = queue.Queue()
+
+    dut.i_start.value = 0
+    dut.i_duty.value = 0
+    dut.i_period.value = 0
+    dut.i_repeat_count.value = 0
+
+    cocotb.start_soon(Clock(dut.i_clk, 10, units="ns").start())
+
+    dut.i_rst_n.value = 0
+    await Timer(20, units="ns")
+    dut.i_rst_n.value = 1
+    await Timer(20, units="ns")
+
+    await Timer(5, units="ns")
+
+    dut.i_start.value = 1
+    dut.i_duty.value = 7
+    dut.i_period.value = 17
+    dut.i_repeat_count.value = 5
+
+    await Timer(20, units="ns")
+
+    await Timer(2500, units="ns")
