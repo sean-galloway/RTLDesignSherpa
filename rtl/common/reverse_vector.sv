63b614c49abf76891d3c90a8e4082f4aa4490dfc
sean-galloway <sean.galloway@outlook.com>
2023-08-13 22:10:46 -0700

    naming changes

---
+++ rtl/common/reverse_vector.sv
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+
+module reverse_vector #(parameter WIDTH=32) (
+    input         [WIDTH-1:0] i_vector,
+    output logic  [WIDTH-1:0] o_vector
+);
+
+    always_comb begin
+        for (integer i=0; i<WIDTH; i++) begin
+            o_vector[(WIDTH-1)-i] = i_vector[i];
+        end
+    end
+
+endmodule : reverse_vector
